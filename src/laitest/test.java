a18c8910004d109184f2516b6d4cde21bb99f997
Administrator <Administrator@BF-201911110845>
2020-10-13 11:13:42 +0800

    cesi

---
+++ src/laitest/test.java
@@ -0,0 +1,11 @@
+package laitest;
+/** 
+* @author LaiJianye: 
+* @version Create-Time：Oct 13, 2020 11:04:04 AM 
+* 类说明 
+*/
+public class test {
+	public static void main(String[] args) {
+		System.out.println("20201013");
+	}
+}

7619d619c6685eeb8a5a1297990200fff0ca8399
lovelaijianye <704016438@qq.com>
2020-10-13 14:14:26 +0800

    增加zhangsan类

---
+++ src/laitest/zhangsan.java
@@ -0,0 +1,11 @@
+package laitest;
+/** 
+* @author LaiJianye: 
+* @version Create-Time：Oct 13, 2020 2:13:23 PM 
+* 类说明 
+*/
+public class zhangsan {
+	public static void main(String[] args) {
+		
+	}
+}
